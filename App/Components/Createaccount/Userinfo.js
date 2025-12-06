cd6ccf60cf8c48a5bad958fdf2e87be80a824f0b
Ammara Hamayou <ammarahamayou@gmail.com>
2025-12-06 16:38:49 +0500

    Initial commit

---
+++ App/Components/Createaccount/Userinfo.js
@@ -0,0 +1,62 @@
+// screens/PhoneScreen.js
+import React, { useState } from "react";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ScaledSheet } from "react-native-size-matters";
+import ProgressBar  from "../Progressbar";
+
+export default function PhoneScreen({ navigation }) {
+  const [phone, setPhone] = useState("");
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ProgressBar active={1} />
+
+      <Text style={styles.title}>What's your phone number?</Text>
+
+      <View style={styles.phoneInputWrapper}>
+        <Text style={styles.countryCode}>+1</Text>
+        <TextInput
+          style={styles.phoneInput}
+          keyboardType="numeric"
+          placeholder="1234567890"
+          placeholderTextColor="#777"
+          value={phone}
+          onChangeText={setPhone}
+        />
+      </View>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate("Namefields")}
+      >
+        <Text style={styles.btnText}>Verify Phone Number</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+}
+
+const styles = ScaledSheet.create({
+  container: { flex: 1, backgroundColor: "#000", padding: "20@ms" },
+  title: { color: "#fff", fontSize: "22@ms", marginTop: "20@ms" },
+  phoneInputWrapper: {
+    flexDirection: "row",
+    backgroundColor: "#1C1C1E",
+    paddingHorizontal: "15@ms",
+    height: "50@ms",
+    borderRadius: "8@ms",
+    alignItems: "center",
+    marginTop: "25@ms",
+  },
+  countryCode: { color: "#fff", fontSize: "16@ms", marginRight: "10@ms" },
+  phoneInput: { flex: 1, color: "white", fontSize: "16@ms" },
+  button: {
+    height: "50@ms",
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: "10@ms",
+    marginTop: "40@ms",
+  },
+  btnText: { fontSize: "16@ms", fontWeight: "700" },
+});
---
+++ App/Components/Createpin/Createpin.js
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  TextInput,
+  Image,
+} from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
+
+export default function CreatePinScreen({ navigation }) {
+  const [pin, setPin] = useState("");
+
+  const handleInput = (value) => {
+    if (pin.length < 4) {
+      const newPin = pin + value;
+      setPin(newPin);
+
+      if (newPin.length === 4) {
+        navigation.navigate("Confirmpin", { firstPin: newPin });
+      }
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      {/* Back Arrow */}
+      <TouchableOpacity onPress={() => navigation.goBack()}>
+        <Icon name="arrow-back" size={26} color="#fff" />
+      </TouchableOpacity>
+
+      {/* Dice Image */}
+      <Image source={require("../../src/images/pin1.png")} style={styles.dice} />
+
+      <Text style={styles.title}>Setup PIN code</Text>
+      <Text style={styles.subtitle}>Setup PIN code to quickly and securely log in</Text>
+
+      {/* PIN Boxes */}
+      <View style={styles.pinRow}>
+        {[0, 1, 2, 3].map((i) => (
+          <View key={i} style={styles.pinBox}>
+            <Text style={styles.pinText}>{pin[i] ? "•" : ""}</Text>
+          </View>
+        ))}
+      </View>
+
+      {/* Hidden Input */}
+      <TextInput
+        style={{ height: 0, width: 0 }}
+        keyboardType="number-pad"
+        autoFocus
+        value={pin}
+        onChangeText={(t) => handleInput(t.slice(-1))}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+    padding: 20,
+  },
+  dice: {
+    width: 100,
+    height: 100,
+    marginTop: 40,
+    alignSelf: "center",
+    marginTop: 100,
+  },
+  title: {
+    color: "#fff",
+    textAlign: "center",
+    fontSize: 22,
+    marginTop: 20,
+  },
+  subtitle: {
+    color: "#888",
+    textAlign: "center",
+    fontSize: 13,
+    marginBottom: 40,
+  },
+  pinRow: {
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: 15,
+  },
+  pinBox: {
+    width: 50,
+    height: 50,
+    borderRadius: 6,
+    backgroundColor: "#1A1A1A",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  pinText: {
+    color: "#fff",
+    fontSize: 28,
+  },
+});
